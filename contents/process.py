6ee4227c4ec2083b9b7be1021f48c0afbdabe9f5
Nicolas git kraken <nicolas.thaize@ynov.com>
2023-01-02 17:30:26 +0100

    refactoring chart 1

--- contents/process.py
+++ contents/process.py
@@ -1,23 +1,41 @@
 import pandas as pd
-import datetime
 from contents.utils import holidays_dates
-import streamlit as st
-
-
-@st.cache
-def load_data():
-    df_cas_france = pd.read_csv('data/covid-cas-france.csv', delimiter=',')
-    return df_cas_france.shape
+import plotly.express as px
+from contents.utils import covid_phases
 
 # Processing data manipulation to return chart 1 final dataframe
-def chart_1_global(mask=True):
+def get_chart_1_data(mask=True):
     df_cas_france = pd.read_csv('data/covid-cas-france.csv', delimiter=',') # Getting data from csv file might be replaced by API call
     chart_1_data = df_cas_france.loc[df_cas_france['pos'].notnull(), ['date', 'pos']] # Selecting non null necessary features
     chart_1_data['date'] = pd.to_datetime(chart_1_data['date'], format='%Y/%m/%d') # Converting date strings to date type
     chart_1_data['day'] = chart_1_data['date'].dt.day_name() # Adding a day name feature for each rows
    
     except_days = ['Saturday', 'Sunday', 'Monday'] 
-    chart_1_data_no_we = chart_1_data.set_index('date') if mask else chart_1_data.loc[~chart_1_data['day'].isin(except_days)].set_index('date') # Excepting rows that are on mondays, saturdays and sundays
+    chart_1_data_no_we = chart_1_data.set_index('date') if mask else chart_1_data.loc[~chart_1_data['day'].isin(except_days)].set_index('date') # Excepting rows that are on mondays, saturdays and sundays if mask == False
     
     chart_mask = (~chart_1_data_no_we.index.strftime('%d%m').isin(holidays_dates)) # Formatting mask to keep only rows that are not corresponding to holiday dates   
     return chart_1_data_no_we.loc[chart_mask] # Final chart data
+
+# Processing plotly manipulations to return chart 1 plot
+def process_chart_1(chart_1_global_data):
+    chart_1 = px.line(
+        chart_1_global_data.reset_index(), 
+        x="date", 
+        y="pos", 
+        title='Nombre de nouveaux cas en France',
+        labels={
+                        "date": "Date",
+                        "pos": "Nombre de cas",
+        }
+    )
+    for covid_phase in covid_phases: # Adding vertical spans/lines for each covid phase
+        if covid_phase['type'] == 'span':
+            chart_1.add_vrect(x0=covid_phase['min'], x1=covid_phase['max'], line_width=0, fillcolor=covid_phase['color'], opacity=0.3, annotation_text=covid_phase['label'], annotation_textangle=90)
+        else:
+            chart_1.add_vline(x=covid_phase['min'], line_color=covid_phase['color'])
+            chart_1.add_annotation(
+                x=covid_phase['min'],
+                text=covid_phase['label'],
+                textangle=270,
+            )
+    return chart_1
--- index.py
+++ index.py
@@ -1,45 +1,19 @@
 import streamlit as st
-import plotly.express as px
-import  streamlit_toggle as tog
+import streamlit_toggle as tog
 
-from contents.process import load_data, chart_1_global
-from contents.sides import chart_1_slide_start_date, chart_1_slide_end_date
-from contents.utils import covid_phases
+from contents.process import get_chart_1_data, process_chart_1
 
 st.title('Data Storytelling Covid19 - Énergies')
-
-st.text(load_data())
-
 
 # CHART 1
 chart_1_toggle = tog.st_toggle_switch(
     label="Afficher les lundi, vendredi et samedi", 
     key="Key1", 
     default_value=False, 
-    label_after = False, 
     inactive_color = '#D3D3D3', 
     active_color="#11567f", 
     track_color="#29B5E8"
-)
-chart_1_global_data = chart_1_global(chart_1_toggle) # Loading whole chart data
-chart_1 = px.line(
-    chart_1_global_data.reset_index(), 
-    x="date", 
-    y="pos", 
-    title='Nombre de nouveaux cas en France',
-    labels={
-                     "date": "Date",
-                     "pos": "Nombre de cas",
-    }
-)
-for covid_phase in covid_phases:
-    if covid_phase['type'] == 'span':
-        chart_1.add_vrect(x0=covid_phase['min'], x1=covid_phase['max'], line_width=0, fillcolor=covid_phase['color'], opacity=0.3, annotation_text=covid_phase['label'], annotation_textangle=90)
-    else:
-        chart_1.add_vline(x=covid_phase['min'], line_color=covid_phase['color'])
-        chart_1.add_annotation(
-            x=covid_phase['min'],
-            text=covid_phase['label'],
-            textangle=270,
-        )
+) # Streamlit toggler 
+chart_1_global_data = get_chart_1_data(chart_1_toggle) # Loading whole chart data
+chart_1 = process_chart_1(chart_1_global_data)
 st.plotly_chart(chart_1)
